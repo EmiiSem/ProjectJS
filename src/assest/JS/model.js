dac8176c1deec7aa8bc0acadc9fc11a2e4642fd5
EmiiSem <just_dragon29@mail.ru>
2024-12-14 01:10:23 +0300

    Optimization application

--- src/assest/JS/model.js
+++ src/assest/JS/model.js
@@ -1,12 +1,16 @@
+import image from '../images/image.png'
+
 const modelArray = [
-    {type: 'title', value: 'Hello World from JS'},
+    {type: 'title', value: 'Hello World from JS', option: {
+        tag: 'h2'
+    }},
     {type: 'text', value: 'here we go with some text'},
     {type: 'description', value: [
         '11111111',
         '22222222',
         '33333333'
     ]},
-    {type: 'image', value: 'assest/images/image.png'}
+    {type: 'image', value: image}
 ]
 
 export { modelArray }
--- src/assest/JS/templates.js
+++ src/assest/JS/templates.js
@@ -1,36 +1,20 @@
+import {row, col} from '../../utilites'
+
 function title(block) {
-    return `
-     <div class="row">
-        <div class="col-sm">
-            <h1>${block.value}</h1>
-        </div>
-    </div>
-    `
+    return row(col(`<h1>${block.value}</h1>`))
 }
 
 function text_text(block) {
-    return `
-    <div class="row">
-        <div class="col-sm">
-            <p>${block.value}</p>
-        </div>
-    </div>
-    `
+    return row(col(`<p>${block.value}</p>`))
 }
 
 function description(block) {
-    const text = block.value.map(item => `<div class="col-sm">${item}</div>`)
-    return `
-    <div class="row">
-        ${text.join('')}
-    </div>
-    `
+    const text = block.value.map(col).join("")
+    return row(`<p>${text}</p>`)
 }
 
 function image(block) {
-    return `
-    <div class="row"><img src="${block.value}"></div>
-    `
+    return row(`<img src="${block.value}">`)
 }
 
 export const templates = {
