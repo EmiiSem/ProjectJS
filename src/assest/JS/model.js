e980d4f89d2adba9f91ddb69b21d69ff00dab981
EmiiSem <just_dragon29@mail.ru>
2024-12-16 20:18:02 +0300

    Refactoring models

--- src/assest/JS/model.js
+++ src/assest/JS/model.js
@@ -1,7 +1,8 @@
 import image from '../images/image.png'
+import { Block } from '../classes/blocks'
 
 const modelArray = [
-    {type: 'title', value: 'Website builder in pure JavaScript', option: {
+    new Block('title', 'Website builder in pure JavaScript', {
         tag: 'h2',
         styles: {
             background: 'linear-gradient(to right, #350431, #433240)',
@@ -9,8 +10,9 @@ const modelArray = [
             padding: '1.5rem',
             'text-align': 'center'
         }
-    }},
-    {type: 'text', value: 'This site was created for an informative purpose and to create certain functions', option: {
+    }),
+
+    new Block('text', 'This site was created for an informative purpose and to create certain functions', {
         styles: {
             'text-align': 'center',
             background: '#000',
@@ -18,25 +20,28 @@ const modelArray = [
             padding: '1rem',
             'font-size': '18px'
         }
-    }},
-    {type: 'description', value: [
+    }),
+
+    new Block('description', [
         'I am a junior+ developer, I have been doing web development for 2-3 years now, but I started my professional career on March 1, 2024',
         'My technology stacks are: HTML, CSS, JavaScript, PHP, and CMS: 1C-Bitrix',
         'The IDEs I use are: Visual Studio Code, PhpStorm, Figma and a bit of PyCharm'
-    ], option: {
+    ], {
         styles: {
             'text-align': 'center',
             'font-weight': 'bold',
             'font-style': 'italic'
         }
-    }},
-    {type: 'image', value: image, option: {
+    }),
+
+    new Block('image', image, {
         styles: {
             display: 'flex',
             'justify-content': 'center',
             padding: '1rem'
         }
-    }}
+    }),
+    
 ]
 
 export { modelArray }
