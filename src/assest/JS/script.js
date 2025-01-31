661821c6e3ebd43d4afeccbdde3992c3554201ce
EmiiSem <just_dragon29@mail.ru>
2025-02-01 01:23:44 +0300

    The script has been changed.js, the template file has been deleted.js, because it is no longer in use. The entire template is stored in the toHtml method

--- src/assest/JS/script.js
+++ src/assest/JS/script.js
@@ -1,17 +1,8 @@
 import { modelArray } from "./model"
-import { templates } from "./templates";
 import "../CSS/style.css"
 
 const $sit = document.querySelector('#sit')
 
 modelArray.forEach(block => {
-    const toHTML = templates[block.type];
-
-    if (typeof toHTML !== 'function') {
-        // console.error(`No template function found for type: ${block.type}`); // Отладка
-        // console.log(`Available templates:`, Object.keys(templates)); // Отладка
-        return;
-    }
-
-    $sit.insertAdjacentHTML('beforeend', toHTML(block));
+    $sit.insertAdjacentHTML('beforeend', block.toHTML());
 });
